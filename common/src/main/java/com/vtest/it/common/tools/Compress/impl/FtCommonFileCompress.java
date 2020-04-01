a17a05d05bc3acff3752acaa87c758714f2b3616
shawn <Mailsunguowei@163.com>
2020-04-01 17:36:58 +0800

    version 1.3 perfect ft compress

--- common/src/main/java/com/vtest/it/common/tools/Compress/impl/FtCommonFileCompress.java
+++ common/src/main/java/com/vtest/it/common/tools/Compress/impl/FtCommonFileCompress.java
@@ -4,8 +4,8 @@ import com.vtest.it.common.pojo.FtStdfInformationBean;
 import com.vtest.it.common.tools.Compress.FtFileCompress;
 import com.vtest.it.common.tools.PerfectFileName;
 import com.vtest.it.common.tools.ftdatalogtools.FtDatalogFileNameParser;
-import org.apache.commons.io.FileUtils;
 import org.zeroturnaround.zip.ZipUtil;
+import org.zeroturnaround.zip.commons.FileUtils;
 
 import java.io.File;
 import java.util.LinkedList;
@@ -19,10 +19,10 @@ public class FtCommonFileCompress implements FtFileCompress {
         ExecutorService service = Executors.newFixedThreadPool(3);
         List<Future<String>> list=new LinkedList<Future<String>>();
         while (!queue.isEmpty()) {
+            final File dataLog = queue.poll();
             Future<String> future = service.submit(new Callable<String>() {
                 @Override
                 public String call() throws Exception {
-                    File dataLog = queue.poll();
                     if (null != dataLog) {
                         String fileName = PerfectFileName.remove(PerfectFileName.removeBrackets(dataLog.getName()));
                         FtStdfInformationBean bean = parser.parse(fileName);
@@ -33,6 +33,7 @@ public class FtCommonFileCompress implements FtFileCompress {
                         File backupFile = new File(backupDirectory.getPath() + "/" + fileName + ".zip");
                         System.out.println(dataLog.getName());
                         ZipUtil.packEntry(dataLog, backupFile);
+                        FileUtils.copyFile(dataLog, new File(backupPath + "/" + dataLog.getName()));
                         if (ZipUtil.containsEntry(backupFile, dataLog.getName())) {
                             fileNeedUploadQueue.offer(backupFile);
                             FileUtils.forceDelete(dataLog);
